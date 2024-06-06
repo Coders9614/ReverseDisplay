b75e95ea6fa72c3d85babe3f835478e3350cc39c
Coders9614 <joonyong.kim1@outlook.com>
2024-06-06 07:26:28 -0600

    Add project files.

---
+++ ReverseDisplay/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecurseNumber
+{
+    internal class Program
+    {
+        static void PrintNaturalNumbers(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                PrintNaturalNumbers(n - 1); // Recur for the previous number
+
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("How many numbers to print: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Expected Output:");
+            PrintNaturalNumbers(n);
+            Console.WriteLine(); // Print a newline after the numbers
+            Console.ReadKey();
+        }
+
+    }
+}
